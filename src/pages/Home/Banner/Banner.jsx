cce6180e9540436b8523d832fc40b71790f58d59
Shuvo Deb <shuvodeb17014@gmail.com>
2023-05-18 00:04:41 +0600

    tailwind and daisyUI install navbar and banner style

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import image1 from '../../../assets/images/Banner/banner1.jpg'
+import image2 from '../../../assets/images/Banner/banner2.jpg'
+import image3 from '../../../assets/images/Banner/banner3.jpg'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full" style={{ height: '86vh' }}>
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src={image1} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src={image2} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={image3} className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
