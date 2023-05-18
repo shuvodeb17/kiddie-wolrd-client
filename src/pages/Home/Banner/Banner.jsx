33060042a22726ec831061d87d0cb40d69d5910d
Shuvo Deb <shuvodeb17014@gmail.com>
2023-05-18 10:33:16 +0600

    gallery section added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -39,7 +39,7 @@ const Banner = () => {
                     </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
-                        <a href="#slide3" className="btn btn-circle">❯</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
---
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import './Gallery.css'
+import image1 from '../../../assets/images/Gallery/gallery1.jpg';
+import image2 from '../../../assets/images/Gallery/gallery2.png';
+import image3 from '../../../assets/images/Gallery/gallery3.png';
+import image4 from '../../../assets/images/Gallery/gallery4.jpg';
+import image5 from '../../../assets/images/Gallery/gallery5.png';
+import image6 from '../../../assets/images/Gallery/gallery6.png';
+
+import LightGallery from 'lightgallery/react';
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+
+
+const Gallery = () => {
+    const onInit = () => {
+        console.log('lightGallery has been initialized');
+    };
+
+
+    return (
+        <div className='mt-5 mb-7'>
+            <div className="container mx-auto">
+                <h1 className='text-3xl font-bold text-center mt-5  mb-7'>Gallery</h1>
+                <div className="App ">
+                    <div className='grid grid-cols-3 gap-5'
+                        onInit={onInit}
+                        speed={500}
+                        plugins={[lgThumbnail, lgZoom]}
+                    >
+                        <a href={image1}>
+                            <img alt="img1" src={image1} />
+                        </a>
+                        <a href={image2}>
+                            <img alt="img2" src={image2} />
+                        </a>
+                        <a href={image3}>
+                            <img alt="img2" src={image3} />
+                        </a>
+                        <a href={image4}>
+                            <img alt="img2" src={image4} />
+                        </a>
+                        <a href={image5}>
+                            <img alt="img2" src={image5} />
+                        </a>
+                        <a href={image6}>
+                            <img alt="img2" src={image6} />
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Gallery;
